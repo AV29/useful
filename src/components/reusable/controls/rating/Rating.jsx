d4f1c9adc4877d3c23c971695667f80c7f38aad7
Anton Vlasik <snumber29@gmail.com>
2019-02-19 00:20:06 +0300

    Tweaked rating control

--- src/components/reusable/controls/rating/Rating.jsx
+++ src/components/reusable/controls/rating/Rating.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { number, string, func } from 'prop-types';
 import { StyledRatingControl, StyledRatingItem, ItemsWrapper } from './styles';
 
-function Rating ({ label, scaleSize, className, icon, value, onChange, iconSize }) {
-  return (
-    <StyledRatingControl className={className}>
-      {label && <div className="label">{label}</div>}
-      <ItemsWrapper>
-        {[...new Array(scaleSize)].map((_, val) => (
-          <StyledRatingItem
-            key={val}
-            icon={icon}
-            size={iconSize}
-            rated={value > val}
-            onClick={() => onChange(val + 1)}
-          />
-        ))}
-      </ItemsWrapper>
-    </StyledRatingControl>
-  );
+class Rating extends Component {
+  constructor(props) {
+    super(props);
+
+    this.previousValue = this.props.value;
+
+  }
+
+  handleClick(val) {
+    this.previousValue === val ?
+      this.props.onChange(val) :
+      this.props.onChange(val + 1);
+
+    this.previousValue = val;
+  }
+
+  render() {
+    const { label, scaleSize, className, icon, value, iconSize } = this.props;
+    return (
+      <StyledRatingControl className={className}>
+        {label && <div className="label">{label}</div>}
+        <ItemsWrapper>
+          {[...new Array(scaleSize)].map((_, val) => (
+            <StyledRatingItem
+              key={val}
+              icon={icon}
+              size={iconSize}
+              rated={value > val}
+              onClick={() => this.handleClick(val)}
+            />
+          ))}
+        </ItemsWrapper>
+      </StyledRatingControl>
+    );
+  }
 }
 
 Rating.propTypes = {
