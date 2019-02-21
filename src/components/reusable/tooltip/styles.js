74f476e686be664e200ea7168f576c5d87f4f511
AV29 <snumber29@gmail.com>
2019-02-21 12:28:44 +0300

    tweaked styles for tooltips for suppoerting theming

--- src/components/reusable/tooltip/styles.js
+++ src/components/reusable/tooltip/styles.js
@@ -1,54 +1,6 @@
 import styled, { css } from 'styled-components';
 
-const minTooltipWidth = 150;
 const tooltipBgColor = 'rgba(0, 0, 0, 0.8)';
-
-const fadeInStyles = `
-  transition: opacity .3s ease;
-  opacity: 1;
-`;
-
-const fadeOutStyles = 'opacity: 0;';
-
-const leftStyles = `
-  box-shadow: 5px 0 20px rgba(120, 120, 120, 1);
-  &:after {
-    top: 50%;
-    left: 100%;
-    transform: translateY(-50%);
-    border-color: transparent transparent transparent ${tooltipBgColor};
-  }
-`;
-
-const rightStyles = `
-  box-shadow: -5px 0 20px rgba(120, 120, 120, 1);
-  &:after {
-    top: 50%;
-    right: 100%;
-    transform: translateY(-50%);
-    border-color: transparent ${tooltipBgColor} transparent transparent;
-  }
-`;
-
-const topStyles = `
-  box-shadow: 0 5px 20px rgba(120, 120, 120, 1);
-  &:after {
-    top: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-    border-color: ${tooltipBgColor} transparent transparent transparent;
-  }
-`;
-
-const bottomStyles = `
-  box-shadow: 0 -5px 20px rgba(120, 120, 120, 1);
-  &:after {
-    bottom: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-    border-color: transparent transparent ${tooltipBgColor} transparent;
-  }
-`;
 
 export const StyledTooltip = styled.div`
   position: absolute;
@@ -59,7 +11,7 @@ export const StyledTooltip = styled.div`
   font-size: 1.4rem;
   color: white;
   pointer-events: none;
-  min-width: ${minTooltipWidth}px;
+  min-width: 150px;
   
   &:after {
     content: " ";
@@ -67,9 +19,64 @@ export const StyledTooltip = styled.div`
     border-style: solid;
   }
   
-  ${({ fading }) => fading ? css`${fadeInStyles}` : css`${fadeOutStyles}`} 
-  ${({ position }) => position === 'left' ? css`${leftStyles}` : ''} 
-  ${({ position }) => position === 'right' ? css`${rightStyles}` : ''} 
-  ${({ position }) => position === 'top' ? css`${topStyles}` : ''} 
-  ${({ position }) => position === 'bottom' ? css`${bottomStyles}` : ''} 
+  ${getFadeStyles} 
+  ${getTooltipStyles} 
 `;
+
+function getFadeStyles ({ fading }) {
+  return fading ?
+    css`transition: opacity .3s ease; opacity: 1;` :
+    css`opacity: 0;`;
+}
+
+function getTooltipStyles ({ position, theme: { shadowColor } }) {
+  switch (position) {
+    case 'left': {
+      return css`
+          box-shadow: 5px 0 20px ${shadowColor};
+          &:after {
+            top: 50%;
+            left: 100%;
+            transform: translateY(-50%);
+            border-color: transparent transparent transparent ${tooltipBgColor};
+          }
+        `;
+    }
+
+    case 'right': {
+      return css`
+          box-shadow: -5px 0 20px ${shadowColor};
+          &:after {
+            top: 50%;
+            right: 100%;
+            transform: translateY(-50%);
+            border-color: transparent ${tooltipBgColor} transparent transparent;
+          }
+        `;
+    }
+
+    case 'top': {
+      return css`
+          box-shadow: 0 5px 20px ${({ theme: { shadowColor } }) => shadowColor};
+          &:after {
+            top: 100%;
+            left: 50%;
+            transform: translateX(-50%);
+            border-color: ${tooltipBgColor} transparent transparent transparent;
+          }
+        `;
+    }
+
+    default: {
+      return css`
+          box-shadow: 0 -5px 20px ${({ theme: { shadowColor } }) => shadowColor};
+          &:after {
+            bottom: 100%;
+            left: 50%;
+            transform: translateX(-50%);
+            border-color: transparent transparent ${tooltipBgColor} transparent;
+          }
+        `;
+    }
+  }
+}
