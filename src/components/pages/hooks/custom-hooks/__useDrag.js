b6feb0f11fca11f5b403fd41aabcc923eecdd8b2
AV29 <snumber29@gmail.com>
2019-02-22 11:35:06 +0300

    Added some experimental hooks

---
+++ src/components/pages/hooks/custom-hooks/__useDrag.js
@@ -0,0 +1,50 @@
+import { useRef, useEffect }  from 'react';
+
+const addEvent = (fn, ref = document) => e => ref.addEventListener(e, fn);
+const removeEvent = (fn, ref = document) => e => ref.removeEventListener(e, fn);
+
+const defaultMouseMoveEvents = ['mousemove', 'touchmove'];
+const defaultMouseDownEvents = ['mousedown', 'touchstart'];
+const defaultMouseUpEvents = ['mouseup', 'touchend'];
+
+const useDraggableEvent = (
+  onDragged,
+  mouseMoveEvents = defaultMouseMoveEvents,
+  mouseDownEvents = defaultMouseDownEvents,
+  mouseUpEvents = defaultMouseUpEvents
+) => {
+  const dragRef = useRef();
+
+  useEffect(() => {
+    let mouseOffsetX = 0;
+    let mouseOffsetY = 0;
+
+    const onRefDragged = e => onDragged({
+      left: e.clientX - mouseOffsetX,
+      top: e.clientY - mouseOffsetY
+    });
+
+    const onMouseUp = () => {
+      mouseMoveEvents.forEach(removeEvent(onRefDragged));
+      mouseUpEvents.forEach(removeEvent(onMouseUp));
+    };
+
+    const onMouseDown = ({ clientX, clientY }) => {
+      // Save the click offset inside dragRef
+      const { top, left } = dragRef.current.getBoundingClientRect();
+      mouseOffsetX = clientX - left;
+      mouseOffsetY = clientY - top;
+
+      mouseMoveEvents.forEach(addEvent(onRefDragged));
+      mouseUpEvents.forEach(addEvent(onMouseUp));
+    };
+
+    mouseDownEvents.forEach(addEvent(onMouseDown, dragRef.current));
+
+    return () => {
+      mouseDownEvents.forEach(removeEvent(onMouseDown, dragRef.current));
+    };
+  }, [onDragged, mouseMoveEvents, mouseDownEvents, mouseUpEvents]);
+
+  return dragRef;
+};
---
+++ src/components/pages/hooks/custom-hooks/__useOverflowDetection.js
@@ -0,0 +1,33 @@
+import { useState, useLayoutEffect, useRef } from 'react';
+
+const useOverflowEffect = () => {
+  const ref = useRef();
+  const [isOverflowingX, setOverflowingX] = useState(false);
+  const [isOverflowingY, setOverflowingY] = useState(false);
+
+  const detectOverflow = () => {
+    const {
+      scrollHeight,
+      scrollWidth,
+      clientHeight,
+      clientWidth
+    } = ref.current;
+
+    const overflowingX = scrollWidth > clientWidth;
+    const overflowingY = scrollHeight > clientHeight;
+
+    overflowingX !== isOverflowingX && setOverflowingX(overflowingX);
+    overflowingY !== isOverflowingY && setOverflowingY(overflowingY);
+  };
+
+  useLayoutEffect(detectOverflow);
+
+  useLayoutEffect(() => {
+    const rafID = requestAnimationFrame(detectOverflow);
+    return () => {
+      cancelAnimationFrame(rafID);
+    };
+  });
+
+  return [ref, isOverflowingX, isOverflowingY];
+};
