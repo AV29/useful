eb0fe366f0dd313bf8731bf6fec7b43bfb723293
AV29 <snumber29@gmail.com>
2019-02-23 17:09:39 +0300

    Possible solution for context menu

--- src/components/pages/hooks/ClickOutside.jsx
+++ src/components/pages/hooks/ClickOutside.jsx
@@ -1,41 +1,54 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { shape, number, bool, string, object } from 'prop-types';
 import useClickOutside from './custom-hooks/useClickOutside';
 import { SmallHeading, DemoSection } from '../../../styles/styles';
 import Portal from '../../reusable/portal/Portal';
 
 function ClickOutside () {
-  const [contextMenuCoordinates, setContextMenuCoordinates] = useState(null);
-
-  const ref = useClickOutside({ onClickOutside: () => setContextMenuCoordinates(null) });
 
   return (
     <DemoSection>
-      <SmallHeading
+      <MenuX
+        target={<SmallHeading>Right Click Here to call context menu</SmallHeading>}
+      >
+        <div
+          style={{ width: 100, height: 100, backgroundColor: 'tomato' }}
+        />
+      </MenuX>
+    </DemoSection>
+  );
+}
+
+function MenuX ({ children, target }) {
+  const [contextMenuCoordinates, setContextMenuCoordinates] = useState(null);
+
+  return (
+    <Fragment>
+      <div
         onContextMenu={(event) => {
           event.preventDefault();
           setContextMenuCoordinates({ left: event.clientX, top: event.clientY });
         }}
       >
-        Right Click Here to call context menu
-      </SmallHeading>
+        {target}
+      </div>
       <ContextMenu
-        refProp={ref}
+        onClickOutside={() => setContextMenuCoordinates(null)}
         coords={contextMenuCoordinates}
-      />
-    </DemoSection>
+      >
+        {children}
+      </ContextMenu>
+    </Fragment>
   );
 }
 
-function ContextMenu ({ refProp, isShown, coords }) {
+function ContextMenu ({ children, refProp, isShown, coords, onClickOutside }) {
   if (coords) {
-    const { top, left } = coords;
     return (
       <Portal>
-        <div
-          ref={refProp}
-          style={{ top, left, width: 100, height: 100, backgroundColor: 'tomato', position: 'absolute' }}
-        />
+        <div ref={useClickOutside({ onClickOutside })} style={{ ...coords, position: 'absolute' }}>
+          {children}
+        </div>
       </Portal>
     );
   } else {
